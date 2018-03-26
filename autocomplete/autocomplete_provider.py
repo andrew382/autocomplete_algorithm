b75c5926f19a7dd1c55568f89dd74afdefe24710
andrew382 <andrew.ke.hunter@gmail.com>
2018-03-26 01:00:08 -0400

    create helper functions + unit tests and basic train & getWord methods.

--- autocomplete/autocomplete_provider.py
+++ autocomplete/autocomplete_provider.py
@@ -2,6 +2,8 @@
 """
 
 import string
+from functools import reduce
+from collections import namedtuple
 from autocomplete import candidate as cand
 
 class AutocompleteProvider:
@@ -12,14 +14,23 @@ class AutocompleteProvider:
     def __init__(self):
         """Initalizes AutocompleteProvider object.
         """
-        pass
+        self.memory = {}
 
     def getWords(self, fragment):
         """Returns list of candidates ordered by confidence.
 
         :param str fragment: The word fragment to be autocompleted.
         """
-        pass
+        fragment = fragment.translate(None, string.punctuation).lower()
+        try:
+            memory_node = get_bottom_node(fragment, self.memory)
+        except KeyError:  # occurs when fragment has never been seen before
+            return []
+        candidate_list = get_candidates(fragment, memory_node.memory)
+        if memory_node.confidence > 0:
+            candidate_list.append(cand.Candidate(fragment, 
+                                                 memory_node.confidence))
+        return sorted(candidate_list, key=lambda x: x.confidence, reverse=True)
 
     def train(self, passage):
         """Trains the algorithm with the provided passage.
@@ -27,7 +38,70 @@ class AutocompleteProvider:
         :param str passage: Contains words that the autocomplete algorithm will
         use to train.
         """
-        pass
+        word_list = preprocess(passage)
+        for word in word_list:
+            self.memory = memorize(self.memory, word)
+
+
+class MemoryNode:
+    """Used by AutocompleteProvider to store the confidence and next memorizied
+    letters of the key (a letter).
+    """
+
+    def __init__(self, memory=None, confidence=None):
+        """Initalizes MemoryNode object.
+        """
+        self.memory = memory
+        self.confidence = confidence
+
+    def __eq__(self, other):
+        """Determines if two instances are equal. Overrides the default 
+        implementation.
+        """
+        if isinstance(self, other.__class__):
+            return self.__dict__ == other.__dict__
+        return False
+
+
+def get_candidates(fragment, memory):
+    candidate_list = []
+    for letter, node in memory.iteritems():
+        new_fragment = fragment + letter
+        if node.confidence > 0:
+            candidate_list.append(cand.Candidate(new_fragment, node.confidence))
+        if node.memory:
+            candidate_list += get_candidates(new_fragment, node.memory)
+    return candidate_list
+
+
+def get_bottom_node(fragment, memory):
+    """Goes down the nested dictionaries in memory in a path given by fragment.
+    Returns the MemoryNode at the end of the path.
+
+    :param str fragment: word fragment that specifies the path.
+    :param dict memory: memory dictionary that the word will be added to.
+    """
+    memory_node = memory[fragment[0]]
+    for letter in fragment[1:]:
+        memory_node = memory_node.memory[letter]
+    return memory_node
+
+
+def memorize(memory, word):
+    """Adds a single word to memory.
+
+    :param dict memory: memory dictionary that the word will be added to.
+    :param str word: the word fragment to be added to memory.
+    """
+    letter = word[0]
+    word = word[1:]
+    memory_node = memory.get(letter, MemoryNode({}, 0))
+    if word:
+        memory_node.memory = memorize(memory_node.memory, word)
+    else:
+        memory_node.confidence += 1
+    memory[letter] = memory_node
+    return memory
 
 
 def preprocess(passage):
--- autocomplete/tests/autocomplete_provider_test.py
+++ autocomplete/tests/autocomplete_provider_test.py
@@ -18,7 +18,7 @@ class TestPreprocess(unittest.TestCase):
     def test_preprocess_punctuation(self):
         """Passage contains punctuation.
         """
-        passage = ',has. ! punc#tion?@ here'
+        passage = ',has.  ! punc#tion?@ here'
         correct_answer = ['has', 'punction', 'here']
         output = auto.preprocess(passage)
 
@@ -35,6 +35,85 @@ class TestPreprocess(unittest.TestCase):
         passage = 'Str#ing h1As, 12capitalS!!!, 0123'
         correct_answer = ['string', 'h1as', '12capitals', '0123']
         output = auto.preprocess(passage)
+
+
+class TestMemorize(unittest.TestCase):
+    """Tests the memorize function.
+    """
+
+    def setUp(self):
+        self.abc_memory = {'a': auto.MemoryNode({'b':  # memory of the word abc
+                                auto.MemoryNode({'c': 
+                                auto.MemoryNode({}, 1)}, 0)}, 0)}
+
+    def test_memorize_empty_memory(self):
+        """Tests memorize function with an empty memory dictionary passed.
+        """
+        word = 'abc'
+        output = auto.memorize({}, word)
+        self.assertEqual(output, self.abc_memory)
+
+    def test_memorize_non_empty_memory(self):
+        """Tests memorize function when memory already exists. 
+        """
+        word = 'adc' 
+        correct_answer = {'a': auto.MemoryNode({
+                                'b': auto.MemoryNode({'c': 
+                                    auto.MemoryNode({}, 1)}, 0),
+                                'd': auto.MemoryNode({'c': 
+                                    auto.MemoryNode({}, 1)}, 0)
+                                }, 0)}
+        output = auto.memorize(self.abc_memory, word)
+        self.assertEqual(output, correct_answer)
+
+    def test_memorize_same_word(self):
+        """Tests memorize function when a word is passed that is already in the 
+        memory.
+        """
+        word = 'abc'
+        correct_answer = {'a': auto.MemoryNode({'b': 
+                               auto.MemoryNode({'c': 
+                               auto.MemoryNode({}, 2)}, 0)}, 0)}
+        output = auto.memorize(self.abc_memory, word)
+        self.assertEqual(output, correct_answer)
+
+    def test_memorize_same_prefix(self):
+        """Tests memorize function when a word is passed that is a prefix of a 
+        word already in the memory.
+        """
+        word = 'ab'
+        correct_answer = {'a': auto.MemoryNode({'b': 
+                               auto.MemoryNode({'c': 
+                               auto.MemoryNode({}, 1)}, 1)}, 0)}
+        output = auto.memorize(self.abc_memory, word)
+        self.assertEqual(output, correct_answer)
+
+
+class TestMemorize(unittest.TestCase):
+    """Tests the memorize function.
+    """
+    pass
+
+class TestAutocompleteProvider(unittest.TestCase):
+    """Tests the methods of the AutocompleteProvider class.
+    """
+
+    def test_train_no_memory(self):
+        """Tests the train method when there are no previous memories.
+        """
+        pass
+
+    def test_getWords_example(self):
+        """Tests getWords method on the given example.
+        """
+        passage = 'The third thing that I need to tell you is that this thing \
+        does not think thoroughly.'
+        alg = auto.AutocompleteProvider()
+        alg.train(passage)
+        candidates = alg.getWords('thi')
+        for c in candidates:
+            print c.word
+            print c.confidence
 
 
 if __name__ == '__main__':
